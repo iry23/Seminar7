937261371b057299544c70959c9834854f948a6d
Irina <anatskayairina@gmail.com>
2022-11-11 12:38:54 +0100

    Seminar7 dz added

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный 
+//случайными вещественными числами.
+/*
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+/*
+Console.Write("Input m: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input n: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[lines, columns];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "; ");
+        }
+        Console.WriteLine("");
+    }
+}
+*/
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание, 
+//что такого элемента нет.
+/*
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1 7 -> числа с такими индексами в массиве нет*/
+/*
+Console.Write("Input m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = Convert.ToInt32(new Random().Next(0, 21));
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + "\t  ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input coordinates");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > m && b > n)
+    Console.WriteLine("This element is not in the array");
+else
+{
+    object c = array.GetValue(a, b);
+    Console.WriteLine($"Your element is {c}");
+}
+*/
+//Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+/*
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+/*
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(-20, 20);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+double result = 0;
+for (int j = 0; j < n; j++)
+{
+    result = Average(j);
+    Console.WriteLine($"Average is: {result}");
+}
+double Average(int column)
+{
+    double average = 0;
+    double a = 0;
+    for (int i = 0; i < m; i++)
+    {
+        average += array[i, column];
+    }
+    a = average / m;
+    return a;
+}
+*/
